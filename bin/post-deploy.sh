a8a012cff7d85426740d99baa7f9a566c8c3c679
Austin deBruyn <austin.debruyn@gmail.com>
2017-09-06 22:42:57 -0700

    omit sentry token

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -2,7 +2,9 @@ yarn
 NODE_ENV=production npm run build
 NODE_ENV=production node bin/migrate
 pm2 startOrRestart /var/makes.audio/ecosystem.json --env production
-curl https://sentry.io/api/hooks/release/builtin/213755/3e00f2a5bf9d3c4d3bf48c7af296081c96caace519b30fa0f2baafd668f8aa2a/ \
+
+GIT_COMMIT = `git rev-parse HEAD`
+curl https://sentry.io/api/hooks/release/builtin/213755/$SENTRY_RELEASE_TOKEN/ \
   -X POST \
   -H 'Content-Type: application/json' \
-  -d '{"version": "`git rev-parse HEAD`"}'
+  -d '{"version": "$GIT_COMMIT"}'
