066926d2368f5953be3e128e0965bd035b29f235
Austin deBruyn <austin.debruyn@gmail.com>
2017-09-06 22:47:28 -0700

    git commit

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -3,7 +3,7 @@ NODE_ENV=production npm run build
 NODE_ENV=production node bin/migrate
 pm2 startOrRestart /var/makes.audio/ecosystem.json --env production
 
-GIT_COMMIT = `git rev-parse HEAD`
+export GIT_COMMIT="`git rev-parse HEAD`"
 curl https://sentry.io/api/hooks/release/builtin/213755/$SENTRY_RELEASE_TOKEN/ \
   -X POST \
   -H 'Content-Type: application/json' \
