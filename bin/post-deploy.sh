7685dce9ba0e754357819ee64b52e9322951d89c
Austin deBruyn <austin.debruyn@gmail.com>
2017-09-28 22:24:40 -0700

    create database in deploy

--- bin/post-deploy.sh
+++ bin/post-deploy.sh
@@ -1,4 +1,5 @@
 yarn
 NODE_ENV=production npm run build
+NODE_ENV=production node bin/createDatabase
 NODE_ENV=production node bin/migrate
 pm2 startOrRestart /var/makes.audio/ecosystem.json --env production
